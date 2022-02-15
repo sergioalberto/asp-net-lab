e166af5534666c84d3f6b7e002b2bbc1d982a578
Sergio GQ <sergio.gonzalez@symphony.com>
2022-02-16 00:24:24 +0100

    HSport template page

---
+++ HPlus.Ecommerce/HPlus.Ecommerce/Controllers/Cart.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HPlus.Ecommerce.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HPlus.Ecommerce/HPlus.Ecommerce/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System.Web;
+// using System.Web.Security;
+
+namespace HPlus.Ecommerce.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string username, string password)
+        {
+            /* if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+            {
+                FormsAuthentication.SetAuthCookie(username, false);
+                return Redirect(FormsAuthentication.GetRedirectUrl(username, false));
+            } */
+
+            return View();
+        }
+    }
+}
